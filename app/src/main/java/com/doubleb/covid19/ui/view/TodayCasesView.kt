1d8db26ecf9d481fe6febb90ed4df68b1f734e8d
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-05 01:30:13 -0300

    + create TodayCasesView as a component

---
+++ app/src/main/java/com/doubleb/covid19/ui/view/TodayCasesView.kt
@@ -0,0 +1,59 @@
+package com.doubleb.covid19.ui.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import androidx.cardview.widget.CardView
+import androidx.core.view.marginLeft
+import com.doubleb.covid19.R
+import kotlinx.android.synthetic.main.view_today_cases.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TodayCasesView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : CardView(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val DATE_FORMAT = "(dd/MM/yy)"
+    }
+
+    private var todayCases = 0
+    private var todayDeaths = 0
+
+    init {
+        View.inflate(context, R.layout.view_today_cases, this)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+    fun loading() {
+        today_cases_text_view_newest_results.loading()
+        today_cases_text_view_newest_deaths_results.loading()
+        today_cases_text_view_date.loading()
+    }
+
+    fun todayCases(todayCases: Int) = apply {
+        this.todayCases = todayCases
+    }
+
+    fun todayDeaths(todayDeaths: Int) = apply {
+        this.todayDeaths = todayDeaths
+    }
+
+    fun build() {
+        today_cases_text_view_date.setLoadedText(
+            SimpleDateFormat(
+                DATE_FORMAT,
+                Locale.getDefault()
+            ).format(Date())
+        )
+
+        today_cases_text_view_newest_results.setLoadedText(todayCases.toString())
+        today_cases_text_view_newest_deaths_results.setLoadedText(todayDeaths.toString())
+    }
+
+}
