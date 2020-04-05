67f65ace6f7f1a16ef9afc4e158f48a9b4a6508a
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-05 01:32:49 -0300

    + rename uncommon class to CountryRepository.kt

--- app/src/main/java/com/doubleb/covid19/repository/CountryRepository.kt
+++ app/src/main/java/com/doubleb/covid19/repository/CountryRepository.kt
@@ -2,7 +2,7 @@ package com.doubleb.covid19.repository
 
 import com.doubleb.covid19.network.Covid19DataSource
 
-class HomeRepository(private val covid19DataSource: Covid19DataSource) {
+class CountryRepository(private val covid19DataSource: Covid19DataSource) {
     fun getCountry(country : String) =
         covid19DataSource.getByCountry(country)
 }
