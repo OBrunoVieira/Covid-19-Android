a13476ab31d8b4bb158f2865f793135a55ccb9a2
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-06 18:15:31 -0300

    + adjust number format by locale

--- app/src/main/java/com/doubleb/covid19/extensions/NumberExtensions.kt
+++ app/src/main/java/com/doubleb/covid19/extensions/NumberExtensions.kt
@@ -1,20 +1,23 @@
 package com.doubleb.covid19.extensions
 
 import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
 import kotlin.math.floor
 import kotlin.math.log10
 import kotlin.math.pow
 
 fun Long.shortValue(): String {
+    val decimalFormat = NumberFormat.getNumberInstance(Locale.getDefault()) as DecimalFormat
     val suffix = charArrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
     val value = floor(log10(this.toDouble())).toInt()
     val base = value / 3
 
     return if (value >= 3 && base < suffix.size) {
-        DecimalFormat("#0.0").format(
-            this / 10.0.pow(base * 3.toDouble())
-        ) + suffix[base]
+        decimalFormat.applyPattern("#0.0")
+        decimalFormat.format(this / 10.0.pow(base * 3.toDouble())) + suffix[base]
     } else {
-        DecimalFormat("#,##0").format(this)
+        decimalFormat.applyPattern("#,##0")
+        decimalFormat.format(this)
     }
 }
