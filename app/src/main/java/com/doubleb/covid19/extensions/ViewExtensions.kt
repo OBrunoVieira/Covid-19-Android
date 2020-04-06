13479bc7aa77204752eb9beab10a4803f7eb9ac9
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-06 02:48:24 -0300

    + create new extension

---
+++ app/src/main/java/com/doubleb/covid19/extensions/ViewExtensions.kt
@@ -0,0 +1,9 @@
+package com.doubleb.covid19.extensions
+
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.DrawableRes
+
+fun TextView.compoundTopDrawable(@DrawableRes drawableRes: Int) {
+    setCompoundDrawablesRelativeWithIntrinsicBounds(0, drawableRes, 0, 0)
+}
