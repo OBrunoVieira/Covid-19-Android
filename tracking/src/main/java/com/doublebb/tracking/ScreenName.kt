8796a66ad5b35fcd8043756720c22b034eed33d3
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-14 23:44:09 -0300

    + map new screen names

--- tracking/src/main/java/com/doublebb/tracking/ScreenName.kt
+++ tracking/src/main/java/com/doublebb/tracking/ScreenName.kt
@@ -4,10 +4,11 @@ import androidx.annotation.Size
 enum class ScreenName(@Size(min = 1L, max = 36L) val value: String) {
     SPLASH("Splash"),
     HOME("Home"),
-    WORLD("World"),
     SEARCH("Search"),
     NEWS("News"),
     PREVENTIONS("Preventions"),
     SYMPTOMS("Symptoms"),
     COUNTRY_DETAILS("Country Details"),
+    OVERVIEW("Overview"),
+    COUNTRIES("Countries")
 }
