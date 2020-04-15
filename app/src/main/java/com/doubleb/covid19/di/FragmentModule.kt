ba6ad810fdcd8f9c40feb658c7f228f6e6986a49
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-14 23:47:35 -0300

    + update DI structure

--- app/src/main/java/com/doubleb/covid19/di/FragmentModule.kt
+++ app/src/main/java/com/doubleb/covid19/di/FragmentModule.kt
@@ -4,9 +4,18 @@ import com.doubleb.covid19.ui.fragment.*
 import org.koin.dsl.module
 
 val fragmentModule = module {
+    // Home Section ================================================================================
     factory { HomeFragment() }
+
+    // World Section ===============================================================================
     factory { WorldFragment() }
+    factory { OverviewFragment() }
+    factory { CountriesFragment() }
+
+    // News Section ================================================================================
     factory { NewsFragment() }
+
+    // Care Section ================================================================================
     factory { CareFragment() }
     factory { PreventionFragment() }
     factory { SymptomsFragment() }
