894b34b0d482fbac98bf64d69db7fb8d8a524bc2
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-14 23:41:15 -0300

    - remove unused files

--- app/src/main/java/com/doubleb/covid19/ui/view_holder/WorldTitleViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.doubleb.covid19.ui.view_holder
-
-import android.view.View
-
-class WorldTitleViewHolder(itemView: View) : WorldViewHolder(itemView){
-
-}
--- app/src/main/java/com/doubleb/covid19/ui/view_holder/WorldViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.doubleb.covid19.ui.view_holder
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-open class WorldViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
