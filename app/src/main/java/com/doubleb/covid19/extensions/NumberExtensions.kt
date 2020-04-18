f364f0e6afdc8c6fcee38a82495b9d5b9e53fcb2
bruno.cruz <bruno.cruz@corp.globo.com>
2020-04-18 17:30:15 -0300

    + add new NumberExtension

--- app/src/main/java/com/doubleb/covid19/extensions/NumberExtensions.kt
+++ app/src/main/java/com/doubleb/covid19/extensions/NumberExtensions.kt
@@ -1,19 +1,20 @@
 package com.doubleb.covid19.extensions
 
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
 import java.util.*
 import kotlin.math.floor
 import kotlin.math.log10
 import kotlin.math.pow
 
-fun Long.shortValue(): String {
+fun Long.shortValue(): String = run {
     val decimalFormat = NumberFormat.getNumberInstance(Locale.getDefault()) as DecimalFormat
     val suffix = charArrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
     val value = floor(log10(this.toDouble())).toInt()
     val base = value / 3
 
-    return if (value >= 3 && base < suffix.size) {
+    return@run if (value >= 3 && base < suffix.size) {
         decimalFormat.applyPattern("#0.0")
         decimalFormat.format(this / 10.0.pow(base * 3.toDouble())) + suffix[base]
     } else {
@@ -21,3 +22,6 @@ fun Long.shortValue(): String {
         decimalFormat.format(this)
     }
 }
+
+fun Double.toPercentString(): String =
+    DecimalFormat("#.##'%'", DecimalFormatSymbols(Locale.getDefault())).format(this)
